5f3e173029eef22a7efa02d06bc1cecd3c5b6001
Ashutosh Bishnoi <mail2bishnoi@gmail.com>
2020-03-27 16:24:24 +0530

    Updates

---
+++ precache-manifest.b136eb79c021f64d1f9b2dce99610ecb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465fd9dd92bf9908d3196299234a8b25",
+    "url": "/exchangerpoint/index.html"
+  },
+  {
+    "revision": "05f0175b7d542283dd91",
+    "url": "/exchangerpoint/static/css/main.8423fc5f.chunk.css"
+  },
+  {
+    "revision": "05065b581f5543d108af",
+    "url": "/exchangerpoint/static/js/2.65428461.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/exchangerpoint/static/js/2.65428461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f0175b7d542283dd91",
+    "url": "/exchangerpoint/static/js/main.9525613c.chunk.js"
+  },
+  {
+    "revision": "ad4af15fe265c002c68f",
+    "url": "/exchangerpoint/static/js/runtime-main.ce389b2c.js"
+  },
+  {
+    "revision": "3ca06f17c729423724cb9950b4fdb362",
+    "url": "/exchangerpoint/static/media/sprite.3ca06f17.svg"
+  },
+  {
+    "revision": "2e13e06d0928f700190b1aa30126e514",
+    "url": "/exchangerpoint/static/media/user.2e13e06d.jpg"
+  }
+]);
